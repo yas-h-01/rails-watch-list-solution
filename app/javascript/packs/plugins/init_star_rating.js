28f8fd8993cafc5ae8c7aad2bae1a0aad8201aa5
Yaser Hussain <yaserhussain5@gmail.com>
2022-01-27 20:12:11 +0000

    day 1 solution

---
+++ app/javascript/packs/plugins/init_star_rating.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'jquery-bar-rating';
+
+const initStarRating = () => {
+  $('#review_rating').barrating({
+      theme: 'css-stars'
+    });
+};
+
+export { initStarRating };
